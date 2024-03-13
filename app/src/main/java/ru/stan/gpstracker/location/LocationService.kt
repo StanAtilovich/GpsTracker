0f5f7049aff7ff65707daa8d7fc8f14624b38648
StanAtilovich <alano92@yandex.ru>
2024-03-13 14:56:09 +0800

    19 start

---
+++ app/src/main/java/ru/stan/gpstracker/location/LocationService.kt
@@ -0,0 +1,25 @@
+package ru.stan.gpstracker.location
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class LocationService: Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+}
